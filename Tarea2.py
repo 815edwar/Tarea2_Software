80f87162a24c9297fb3d828db006f023989b0a6d
Edwar <edwarypz@gmail.com>
2017-01-26 09:34:56 -0400

    Agregada func tarifaDelDia

--- Tarea2.py
+++ Tarea2.py
@@ -8,3 +8,11 @@ class Servicio:
         self.tarifa_fin_de_semana = fin_de_semana
         self.inicio = rango_servicio[0]
         self.fin = rango_servicio[1]
+
+    def tarifaDelDia(self,fecha):
+        dia = fecha.strftime("%a")
+        
+        if (dia =="Sat" or dia =="Sun"):
+            return self.tarifa_fin_de_semana
+        
+        return self.tarifaSemana
